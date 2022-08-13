1e558d183322cb408a57bf67fd5c6f426db6b1a8
Chibuzor Okoroafor <chibuzorokoroafor6@gmail.com>
2022-08-14 00:43:53 +0100

    Capitalize works with punctuaution

--- dist/capitalize.js
+++ dist/capitalize.js
@@ -1,7 +1,16 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const capitalize = (str) => {
-    return str[0].toUpperCase() + str.slice(1);
+    let str2 = str;
+    let capitalized = false;
+    for (let i = 0; i < str2.length; i += 1) {
+        if (str2.charCodeAt(i) >= 97 && str2.charCodeAt(i) <= 122) {
+            str2 = str2.split("");
+            str2.splice(i, 1, str2[i].toUpperCase());
+            str2 = str2.join("");
+            return str2;
+        }
+    }
 };
 exports.default = capitalize;
 //# sourceMappingURL=capitalize.js.map
--- scripts/capitalize.ts
+++ scripts/capitalize.ts
@@ -1,6 +1,15 @@
 
 const capitalize = (str: string) => {
-    return str[0].toUpperCase() + str.slice(1);
+    let str2: string| string[] = str;
+    let capitalized = false;
+    for (let i = 0; i < str2.length; i += 1) {
+        if (str2.charCodeAt(i) >= 97 && str2.charCodeAt(i) <= 122) {
+            str2 = str2.split("");
+            str2.splice(i, 1, str2[i].toUpperCase());
+            str2 = str2.join("");
+            return str2;
+        }
+    }
 }
 
 export default capitalize;
