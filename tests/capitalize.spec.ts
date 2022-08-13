81216212f7d4e9cd3b8cc1f600aa30a28f53c05f
Chibuzor Okoroafor <chibuzorokoroafor6@gmail.com>
2022-08-14 00:45:50 +0100

    Add test for already capitalized word

--- tests/capitalize.spec.ts
+++ tests/capitalize.spec.ts
@@ -12,3 +12,7 @@ test("capitalizes first letter (2)", () => {
 test("works when first character is not a letter", () => {
     expect(capitalize(";nice move")).toBe(";Nice move");
 })
+
+test("works with already capitalized words", () => {
+    expect(capitalize("Robot")).toBe("Robot");
+})
