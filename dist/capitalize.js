e094e274246c4428cbce4a6b531e6854653fee8c
Chibuzor Okoroafor <chibuzorokoroafor6@gmail.com>
2022-08-14 00:48:17 +0100

    Capitalize works on already capitalized words

--- dist/capitalize.js
+++ dist/capitalize.js
@@ -4,7 +4,8 @@ const capitalize = (str) => {
     let str2 = str;
     let capitalized = false;
     for (let i = 0; i < str2.length; i += 1) {
-        if (str2.charCodeAt(i) >= 97 && str2.charCodeAt(i) <= 122) {
+        if (str2.charCodeAt(i) >= 97 && str2.charCodeAt(i) <= 122 ||
+            str2.charCodeAt(i) >= 65 && str2.charCodeAt(i) <= 90) {
             str2 = str2.split("");
             str2.splice(i, 1, str2[i].toUpperCase());
             str2 = str2.join("");
--- scripts/capitalize.ts
+++ scripts/capitalize.ts
@@ -3,7 +3,10 @@ const capitalize = (str: string) => {
     let str2: string| string[] = str;
     let capitalized = false;
     for (let i = 0; i < str2.length; i += 1) {
-        if (str2.charCodeAt(i) >= 97 && str2.charCodeAt(i) <= 122) {
+        if (
+            str2.charCodeAt(i) >= 97 && str2.charCodeAt(i) <= 122 ||
+            str2.charCodeAt(i) >= 65 && str2.charCodeAt(i) <= 90
+        ) {
             str2 = str2.split("");
             str2.splice(i, 1, str2[i].toUpperCase());
             str2 = str2.join("");
